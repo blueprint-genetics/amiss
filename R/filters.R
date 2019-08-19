943cc076471278197e0274acb709436fc8c70a57
M. Särkkä <mikko.sarkka@blueprintgenetics.com>
2019-08-19 15:21:16 +0300

    Add comments and function descriptions

--- R/filters.R
+++ R/filters.R
@@ -1,18 +1,37 @@
 
+#' Check if variants are missense
+#'
+#' @param variant A data.frame containing VEP-annotated variants (must have a "Consequence"-column)
+#'
+#' @return A logical vector with as many values as rows in `variant`. Each value is `TRUE` 
+#' if "missense_variant" appears in the Consequence field, and `FALSE` otherwise.
 missense <- function(variant) {
   
-  stopifnot(class(variant) == "data.frame")# nrow(variant)  == 1)
+  stopifnot(class(variant) == "data.frame")
   
-  grepl("missense_variant", variant[, "Consequence", drop = TRUE], fixed=TRUE)
+  grepl("missense_variant", variant[, "Consequence", drop = TRUE], fixed = TRUE)
 }
 
+#' Check whether variants are ClinGen-reviewed
+#'
+#' @param variant A data.frame containing VEP-annotated variants (must have a "CLNREVSTAT"-column)
+#'
+#' @return A logical vector with as many values as rows in `variant`. Each value is `TRUE` 
+#' if the CLNREVSTAT field is either "reviewed_by_expert_panel" or "practice_guideline", 
+#' and `FALSE` otherwise.
 clingen <- function(variant) {
   
-  stopifnot(class(variant) == "data.frame")#, nrow(variant)  == 1) 
+  stopifnot(class(variant) == "data.frame")
   
   variant[, "CLNREVSTAT", drop = TRUE] %in% c("reviewed_by_expert_panel", "practice_guideline")
 }
 
+#' Check whether transcripts are Ensembl-canonical
+#'
+#' @param variant A data.frame containing VEP-annotated variants (must have a "CANONICAL"-column)
+#'
+#' @return A logical vector with as many values as rows in `variant`. Each value is `TRUE` 
+#' if the CANONICAL field is "YES", and `FALSE` otherwise.
 canonical <- function(transcript) {
   
   stopifnot(class(transcript) == "data.frame")
@@ -20,15 +39,24 @@ canonical <- function(transcript) {
   transcript[, "CANONICAL", drop = TRUE] == "YES"
 }
 
-
+#' Apply a set of filters to a data.frame
+#'
+#' @param filters List or vector of filters (functions that return 
+#' a logical value for each row of a data.frame)
+#' @param df A data.frame
+#'
+#' @return Subset of `df` that passes every filter
 apply_filters <- function(filters, df) {
   
+  # Compute outcome of every filter for df
   filter_vectors <- lapply(X = filters, FUN = function(x) { x(df) })
   
+  # Compute AND pointwise over all filter results
   if (length(filters) > 1) { 
     combined_filter <- Reduce(f = `&`, filter_vectors)
   } 
   else {
+    # If there is only one filter, remove the list-wrap
     combined_filter <- unlist(filter_vectors)
   }
   
--- R/process_data.R
+++ R/process_data.R
@@ -1,4 +1,10 @@
 
+#' Partition dataset into a training and a test set
+#'
+#' @param dataframe A data.frame that will be partitioned into two.
+#' @param proportion Proportional size of training set compared to test set
+#'
+#' @return Two-element list containing the training and test sets.
 split_train_test <- function(dataframe, proportion) {
   
   stopifnot(class(dataframe) == "data.frame")
@@ -8,6 +14,10 @@ split_train_test <- function(dataframe, proportion) {
   stopifnot(proportion < 1.0)
   stopifnot(proportion > 0.0)
   
+  # Compute vector answering "does this element belong to the training set?"
+  # Draw from a uniform distribution between 0 and 1, 
+  # and threshold so that approximately `proportion` rows will
+  # be assigned to the training set
   index <- runif(nrow(dataframe), min = 0, max = 1) < proportion
   
   row.names(dataframe) <- NULL
