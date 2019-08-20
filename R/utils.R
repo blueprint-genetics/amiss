82a55e32b39d2874e1430e7ac673ace4815d26f7
M. Särkkä <mikko.sarkka@blueprintgenetics.com>
2019-08-20 07:55:52 +0300

    Add comments and documentation and remove one redundant function

--- R/utils.R
+++ R/utils.R
@@ -16,12 +16,25 @@ partition_rows_into_batches <- function(num_rows, batches = 100) {
   
 }
 
+#' Form a tidy data.frame of correlations (optionally with hierarchical clustering)
+#'
+#' First and second columns are factors, together representing all the combinations of variables
+#' (with levels ordered according to the clustering), and the third is numeric and contains a 
+#' correlation for the two variables on that row.
+#' 
+#' @param data A data.frame.
+#' @param cluster Whether to perform clustering or not.
+#'
+#' @return Tidy data.frame representing correlations. 
 correlation_tidy <- function(data, cluster = FALSE) {
+  
+  # Compute correlations using available observations
   corr <- cor(data, use = "pairwise.complete.obs")
+  
   if (cluster) {
+    # Order variable names by hierarchical clustering (for ggplot2)
     variables <- colnames(data)
     clustering_order <- hclust(dist(corr))$order
-    corr <- data.frame(corr, stringsAsFactors = TRUE)
     variables <- variables[clustering_order]
   }
   corr <- data.frame(x = row.names(corr), corr, stringsAsFactors = TRUE)
@@ -35,6 +48,13 @@ correlation_tidy <- function(data, cluster = FALSE) {
   return(corr)
 }
 
+#' Form a tidy data.frame of the correlations of missingness indicators 
+#' (optionally with hierarchical clustering).
+#'
+#' @param data A data.frame.
+#' @param cluster Whether to perform clustering or not.
+#'
+#' @return Tidy data.frame representing correlations between missingness indicators.
 missingness_correlation_tidy <- function(data, cluster = FALSE) {
   
   miss_corr <- is.na(data) %>% data.frame
--- R/visualizations.R
+++ R/visualizations.R
@@ -2,25 +2,11 @@ library(ggplot2)
 library(tidyr)
 library(cluster)
 
-to_heatmap_format <- function(data) {
-  
-  variables <- factor(colnames(data), levels=colnames(data))
-  observations <- factor(rownames(data), levels=rownames(data))
-  n_variables <- length(variables)
-  n_observations <- length(observations)
-  
-  data_long <- data.frame(
-    x = rep(observations, times = n_variables), 
-    y = rep(variables, each = n_observations),  
-    stringsAsFactors = FALSE)
-  
-  data_long$value <- mapply(FUN = function(x,y) data[x,y], 
-                            x = data_long$x, 
-                            y = data_long$y)
-  
-  return(data_long)
-}
-
+#' Form a heatmap plot
+#'
+#' @param data Tidy data.frame with `x`, `y` and `value` columns
+#'
+#' @return A ggplot2 object representing a heatmap
 heatmap <- function(data) {
   
   ggplot(data = data, aes(x = x, y = y, fill = value)) + 
