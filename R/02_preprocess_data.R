f7731cea904ee3941a55017e1073a528e60f989c
M. Särkkä <mikko.sarkka@blueprintgenetics.com>
2019-09-09 10:43:58 +0300

    Select useful-looking features from CADD annotations

--- R/02_preprocess_data.R
+++ R/02_preprocess_data.R
@@ -10,6 +10,7 @@ set.seed(20)
 training_set <- read.csv("training_data.csv", as.is = TRUE)
 
 numeric_features <- make.names(c(
+  # From dbNSFP
   "SIFT_score", 
   "LRT_score", 
   "LRT_Omega", 
@@ -29,10 +30,66 @@ numeric_features <- make.names(c(
   "phastCons100way_vertebrate", 
   "phastCons20way_mammalian", 
   "SiPhy_29way_logOdds", 
-  "gnomAD_exomes_AF"
+  "gnomAD_exomes_AF",
+  
+  # From CADD annotations
+  "Length",
+  "GC",
+  "CpG",
+  "motifECount",
+  "motifEHIPos",
+  "motifEScoreChng",
+  # "(Intron)",
+  # "(Exon)",
+  "relcDNApos",
+  "relCDSpos",
+  "relProtPos",
+  "Dst2Splice",
+  "minDistTSS",
+  "minDistTSE",
+  # "targetScan",
+  "mirSVR-Score",
+  "mirSVR-E",
+  "mirSVR-Aln",
+  "tOverlapMotifs",
+  "motifDist",
+  "EncodeH3K4me1-sum",
+  "EncodeH3K4me1-max",
+  "EncodeH3K4me2-sum",
+  "EncodeH3K4me2-max",
+  "EncodeH3K4me3-sum",
+  "EncodeH3K4me3-max",
+  "EncodeH3K9ac-sum",
+  "EncodeH3K9ac-max",
+  "EncodeH3K9me3-sum",
+  "EncodeH3K9me3-max",
+  "EncodeH3K27ac-sum",
+  "EncodeH3K27ac-max",
+  "EncodeH3K27me3-sum",
+  "EncodeH3K27me3-max",
+  "EncodeH3K36me3-sum",
+  "EncodeH3K36me3-max",
+  "EncodeH3K79me2-sum",
+  "EncodeH3K79me2-max",
+  "EncodeH4K20me1-sum",
+  "EncodeH4K20me1-max",
+  "EncodeH2AFZ-sum",
+  "EncodeH2AFZ-max",
+  "EncodeDNase-sum",
+  "EncodeDNase-max",
+  "EncodetotalRNA-sum",
+  "EncodetotalRNA-max",
+  # "Grantham",
+  "RemapOverlapTF",
+  "RemapOverlapCL"
 ))
 categorical_features <- make.names(c(
-  "LRT_pred"
+  # From dbNSFP
+  "LRT_pred",
+  
+  # From CADD annotations
+  "Type",
+  "Dst2SplType"
 ))
 
 training_data <- training_set[, c(numeric_features, categorical_features)]
