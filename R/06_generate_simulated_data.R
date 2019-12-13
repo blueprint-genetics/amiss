6121560fa18f2417cea02ed295748ff2d4b86441
M. Särkkä <mikko.sarkka@blueprintgenetics.com>
2019-12-13 14:01:59 +0200

    Properly deal with dummy variables + comment code

--- R/06_generate_simulated_data.R
+++ R/06_generate_simulated_data.R
@@ -6,6 +6,8 @@ library(doParallel)
 library(doRNG)
 
 source("R/simulation_definitions.R")
+source("R/feature_definitions.R")
+source("R/utils.R")
 
 registerDoParallel(3)
 seed <- 42
@@ -13,9 +15,10 @@ seed <- 42
 training_data <- read.csv("contracted_training_data.csv", as.is = TRUE, row.names = 1)
 
 # Temporary, to make simulations take reasonable time when testing
-repeats <- 3
-ampute_params <- ampute_params[sample(1:NROW(ampute_params), 10), ]
+# repeats <- 3
+# ampute_params <- ampute_params[sample(1:NROW(ampute_params), 10), ]
 
+# Create directories for each repetition
 directories <- sapply(1:repeats, function(i) {
   paste0("output/simulated_data/repeat_", i, "/")
 })
@@ -23,24 +26,62 @@ directories <- sapply(1:repeats, function(i) {
 dir_creation_success <- sapply(directories, function(d) {
   dir.create(d, showWarnings = TRUE, recursive = TRUE)
 })
-if (!all(dir_creation_success)) {
-  stop("Failed to create director(y/ies) for saving output.")
-}
 
 # TODO: informative missingness simulation
 
 ampute_per_pattern <- function(data, ...) {
+
+  # We have to make sure that all dummy variables generated from a categorical
+  # variable are set missing when one is set missing.
+  # For this purpose, we need to find the sets of dummies that were generated from
+  # the same categorical variable.
+  dummy_sets <- lapply(categorical_features, . %>% find_dummies(colnames(data)))
+
+  # Divide the data into sets of rows with the same missingness pattern
   missingness_patterns <- data %>% is.na
   missingness_pattern_factor <- apply(missingness_patterns, MARGIN = 1, function(x) paste0(as.integer(x), collapse = "")) %>% factor
   data_per_mp <- split(data, missingness_pattern_factor)
+
   data_per_mp_amputed <- lapply(data_per_mp, function(td) {
+
+    # Each dataset now has only completely observed and completely missing columns.
+    # Select only the complete variables to run `ampute` on.
     mp <- is.na(td[1,, drop = TRUE])
     complete_section <- td[, !mp, drop = FALSE]
-    amp <- ampute(complete_section, std = FALSE)
+
+    # Next ensure co-occurrence of missingness over same-source dummy variables
+    # Get the missingness patterns that ampute generates by default for this complete set
+    dry_amp <- ampute(complete_section, std = FALSE, run = FALSE)
+    pats <- dry_amp$patterns
+    for (dummy_set in dummy_sets) {
+      if (any(dummy_set %in% colnames(complete_section))) {
+        # If one exists in this pattern, they all should. Any prior missingness
+        # should already be present in each, as they are from the same variable.
+        stopifnot(all(dummy_set %in% colnames(complete_section)))
+
+        pats[,] <- apply(pats, MARGIN = 1, function(row) {
+          # if any dummy variable from this set is to be missing,
+          # set all the others to missing too
+          if (any(row[dummy_set] == 0)) {
+            row[dummy_set] <- 0
+          }
+          return(row)
+        })
+      }
+    }
+
+    # After the changes due to dummy variables, there are likely duplicate patterns.
+    pats <- unique(pats)
+
+    # Finally run `ampute`
+    amp <- ampute(complete_section, patterns = pats, std = FALSE)
     td[, !mp] <- amp$amp
     return(td)
   })
+
+  # Once all subset has been "amputed", collect them back into a single dataset
   amputed_data <- unsplit(data_per_mp_amputed, missingness_pattern_factor)
+
   return(amputed_data)
 }
 
--- R/simulation_definitions.R
+++ R/simulation_definitions.R
@@ -1,13 +1,13 @@
 
 # For MCAR, the type argument makes no difference
 mcar_ampute_params <- list(
-  prop = 1:9 * 0.1, 
+  prop = 1:9 * 0.1,
   mech = c("MCAR"),
   type = c("LEFT")
 ) %>% expand.grid(stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
 
 non_mcar_ampute_params <- list(
-  prop = 1:9 * 0.1, 
+  prop = 1:9 * 0.1,
   mech = c("MAR", "MNAR"),
   type = c("LEFT", "TAIL", "RIGHT", "MID")
   ) %>% expand.grid(stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
