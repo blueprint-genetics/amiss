816c191fbc7d20f4b68a2a6742670a7b530b6a13
Mikko Särkkä <mikko.sarkka@blueprintgenetics.com>
2020-02-06 13:31:52 +0200

    Initial implementation of script to analyze simulation results

---
+++ R/09_analyze_simulation_results.R
@@ -0,0 +1,32 @@
+library(magrittr)
+library(dplyr)
+library(stringr)
+
+sim_data_paths <- read.csv(file = "successfully_simulated_file_list.csv", as.is = TRUE)[,2]
+sim_result_paths <- paste0(sim_data_paths, "_output")
+
+rf_rmses <- file.path(sim_result_paths, "rf_rmse.csv")
+lr_rmses <- file.path(sim_result_paths, "lr_rmse.csv")
+rf_perfs <- file.path(sim_result_paths, "rf_performance.csv")
+lr_perfs <- file.path(sim_result_paths, "lr_performance.csv")
+
+rbind_csvs <- function(paths) {
+  csvs <- lapply(paths, . %>% read.csv(as.is = TRUE))
+  paths_components <- data.frame(str_match(paths, pattern = "repeat_([0-9]+).*(0.[1-9])_([MNCAR]+)_(LEFT|RIGHT|MID|TAIL)")[,-1])
+  colnames(paths_components)[1:4] <- c("repeat", "pct", "mech", "orientation")
+  csvs <- mapply(function(path_ix, csv) {
+    path <- paths_components[rep(path_ix, NROW(csv)),, drop = FALSE]
+    cbind(path, csv)
+  }, path_ix = 1:NROW(paths_components), csv = csvs, SIMPLIFY = FALSE)
+  csv_df <- dplyr::bind_rows(csvs)
+  return(csv_df)
+}
+rf_rmse_df <- rbind_csvs(rf_rmses)
+colnames(rf_rmse_df)[colnames(rf_rmse_df) == "X"] <- "method"
+lr_rmse_df <- rbind_csvs(lr_rmses)
+colnames(lr_rmse_df)[colnames(lr_rmse_df) == "X"] <- "method"
+
+rf_perf_df <- rbind_csvs(rf_perfs)
+lr_perf_df <- rbind_csvs(lr_perfs)
+
+rf_df <- merge(rf_perf_df, rf_rmse_df, by = c("repeat", "pct", "mech", "orientation", "method"), all = TRUE)
