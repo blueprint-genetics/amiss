989102328d8f4dd2f292a3e762fa2568fb4fafc6
Mikko Särkkä <mikko.sarkka@blueprintgenetics.com>
2020-02-20 12:45:47 +0200

    Reduce number of simulated datasets per repetition

--- R/simulation_definitions.R
+++ R/simulation_definitions.R
@@ -1,18 +1,17 @@
 
 # For MCAR, the type argument makes no difference
 mcar_ampute_params <- list(
-  prop = 1:9 * 0.1,
+  prop = 1:4 * 0.2,
   mech = c("MCAR"),
   type = c("LEFT")
 ) %>% expand.grid(stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
 
 non_mcar_ampute_params <- list(
-  prop = 1:9 * 0.1,
+  prop = 1:4 * 0.2,
   mech = c("MAR", "MNAR"),
   type = c("LEFT", "TAIL", "RIGHT", "MID")
   ) %>% expand.grid(stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
 
 ampute_params <- rbind(mcar_ampute_params, non_mcar_ampute_params)
 
-repeats <- 1000
-    
+repeats <- 400
