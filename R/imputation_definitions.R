f50849b3c5527747839facb5a6bb78e67a57f5cb
Mikko Särkkä <mikko.sarkka@blueprintgenetics.com>
2020-06-29 10:21:51 +0300

    Change grids to make imputation methods take approx equal time

--- R/imputation_definitions.R
+++ R/imputation_definitions.R
@@ -1,10 +1,10 @@
 
 mice_imputation_hyperparameters <- list(
 
-  pmm = list(donors = (0:10)*3 + 1, ridge = c(1e-03, 1e-04, 1e-05, 1e-06, 1e-07, 1e-08), matchtype = 0:2),
+  pmm = list(donors = (0:9)*4 + 1, ridge = c(1e-03, 1e-04, 1e-05, 1e-06, 1e-07, 1e-08), matchtype = 0:2),
   norm.predict = list(),
   norm = list(),
-  rf = list(ntree = c(5, 10, 20, 50, 100, 150, 200, 500))
+  rf = list(ntree = (0:35) * 15 + 1)
 )
 
 mice_hyperparameter_grids <- lapply(mice_imputation_hyperparameters, expand.grid)
@@ -12,7 +12,7 @@ mice_hyperparameter_grids <- lapply(mice_imputation_hyperparameters, expand.grid
 other_imputation_hyperparameters <- list(
   knnImputation = list(k = 1:5),
   bpca = list(nPcs = 2:15, maxSteps = 1:8*20),
-  missForest = list(mtry = 1:5 * 8 - 4, ntree = c(10, 50, 100, 200))
+  missForest = list(mtry = 1:3 * 15 - 10, ntree = c(50, 100, 200, 500))
 )
 
 other_hyperparameter_grids <- lapply(other_imputation_hyperparameters, expand.grid)
