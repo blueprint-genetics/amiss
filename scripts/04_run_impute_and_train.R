68359bba39cc80a3e29c2576efeeba8dd6951e5f
M. Särkkä <mikko.sarkka@blueprintgenetics.com>
2024-07-15 10:50:28 +0300

    Make 04 script use package

--- scripts/04_run_impute_and_train.R
+++ scripts/04_run_impute_and_train.R
@@ -1,18 +1,17 @@
 library(here)
+library(magrittr)
+library(amiss)
 
 source(here("R", "constants.R"))
-source(here("R", "impute_and_train.R"))
-source(here("R", "utils.R"))
 source(here("R", "imputation_definitions.R"))
 
-cores <- get_env_cores()
-
-impute_and_train(training_path = here("output", "data", FILE_PREPROCESSED_TRAINING_DATA_CSV),
-                 outcome_path = here("output", "data", FILE_TRAINING_OUTCOMES_CSV),
+impute_and_train(training_path = here("output", FILE_PREPROCESSED_TRAINING_DATA_CSV),
+                 outcome_path = here("output", FILE_TRAINING_OUTCOMES_CSV),
                  output_path = here("output"),
-                 mice_hyperparameter_grids = mice_hyperparameter_grids,
-                 other_hyperparameter_grids = other_hyperparameter_grids,
+                 mice_hyperparameter_grids = list(),
+                 other_hyperparameter_grids = list(),
                  single_value_imputation_hyperparameter_grids = single_value_imputation_hyperparameter_grids,
-                 cores = cores, seed = 10, lean = FALSE)
+                 parameter_list=rjson::fromJSON(file = "combination_orig.json"),
+                 cores = 1, seed = 10, lean = TRUE)
 
 write(capture.output(sessionInfo()), here("output", "04_run_impute_and_train_sessioninfo.txt"))
